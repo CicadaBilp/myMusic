43a0e0bbb82e6735102e332316edf472dbe8bda2
Rouser <2402984246@qq.com>
2021-02-05 00:10:23 +0800

    播放歌曲基本完成,开始歌词解析

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
 <template>
   <div id="app">
     <router-view/>
+      <play-music></play-music>
   </div>
 </template>
 
-<style lang="scss">
+<script>
+import PlayMusic from './views/playmusic/PlayMusic'
+export default {
+  components:{PlayMusic}
+}
+</script>
+
+<style lang="scss" scoped>
+@import "assets/css/global.scss";
+  #app{
+    width: 100%;
+    height: 100%;
+    overflow-x: hidden;
+  }
   
 </style>
